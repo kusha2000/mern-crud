2dd63fd425f24b1e10ec76e2cb60cbf6b32cc8ec
kusha2000 <kushanandarawewa1@gmail.com>
2024-05-17 22:37:17 +0530

    Frontend Created

--- src/Users.js
+++ src/Users.js
@@ -1,5 +1,31 @@
-const Users=()=>{
-    
-}
+import { Box } from "@mui/material";
+import UserForm from "./UserForm";
+import UsersTable from "./UsersTable";
+
+const users = [
+  {
+    id: 1,
+    name: "kushan",
+  },
+  {
+    id: 2,
+    name: "Amali",
+  },
+];
+
+const Users = () => {
+  return (
+    <Box 
+    sx={{
+        width:'calc(100%-100px)',
+        margin:'auto',
+        marginTop:'100px',
+        marginLeft:'50px'
+    }}>
+      <UserForm />
+      <UsersTable rows={users}/>
+    </Box>
+  );
+};
 
 export default Users;
